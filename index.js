8248407e29149f3356287ec0d26c190bc3cf24a6
Alejandro N <alex.nevarez.d@gmail.com>
2025-06-03 11:03:24 -0600

    Modificacion para Actions

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const suma = require('./math');
 
-app.get('/', (req, res) => {res.send('Hola el resultado es: ' + suma(10, 15));
+app.get('/', (req, res) => {res.send('Hola el resultado de la suma es: ' + suma(10, 15));
 });
 
 app.listen(3000, () => {console.log('Servidor escuchando en el puerto 3000');
